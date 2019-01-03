c266d3a52b7dfb56a25bd6dc78f6b32c191f0f2e
Baptiste <baptisteperrin77@gmail.com>
2019-01-03 15:23:55 +0100

    Mise a jour 15h23

---
+++ map.js
@@ -0,0 +1,5 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiYXJtYWRpbmRvbiIsImEiOiJjanFnbmN4bzUwMGNtNDJwcWI0ZjQwMzdlIn0.nXQZ0vYO6txUmUgWgiPTrw';
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v11'
+});
