7909fea01c5ef51cedc404b1d2b49c9640c19af8
Baptiste Perrin <45365108+Armadindon@users.noreply.github.com>
2019-06-13 16:03:09 +0200

    Delete parametres.php

--- php/parametres.php
+++
@@ -1,5 +0,0 @@
-<?php /* Connexion au serveur et à la base de données */
-$host="sqletud.u-pem.fr";
-$user="bperrin"; /* Votre login */
-$pwd="5wai6aiJkq"; /* Votre mot de passe*/
-$db="bperrin_db"; /*Le nom de votre base : de la forme ici : xxx_db avec xxx votre login */ ?>
